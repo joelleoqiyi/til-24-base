99affdda6fbc72d61fef115fc5150d75ca4a99de
joelleoqiyi <joel.leo.qiyi@gmail.com>
2024-05-13 13:47:15 +0000

    Revert "hmm"
    This reverts commit 32d6df938990f45edcddd52adff0a0faee0c4ecb.

--- vlm/src/VLMManager.py
+++ vlm/src/VLMManager.py
@@ -11,5 +11,4 @@ class VLMManager:
 
     def identify(self, image: bytes, caption: str) -> List[int]:
         # perform object detection with a vision-language model
-        
         return [0, 0, 0, 0]
